595602c34154bd2b5552b4a3d32ca54970b88b20
jakubantoszek <jakubantoszek1@gmail.com>
2022-07-07 12:46:15 +0200

    Classify emails

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import file
+import email
 
 if __name__ == '__main__':
     dir_list = os.listdir('emails')
@@ -9,4 +10,11 @@ if __name__ == '__main__':
         f = file.File(file_path)
         f.get_emails_from_file(emails_list)
 
-
+    correct_emails = []
+    incorrect_emails = []
+    for email_text in emails_list:
+        email_obj = email.Email(email_text)
+        if email_obj.correct:
+            correct_emails.append(email_obj)
+        else:
+            incorrect_emails.append(email_obj)
