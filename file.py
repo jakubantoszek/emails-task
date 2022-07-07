770a4b0bdda5d194fcfc14eec532637b180fe41c
jakubantoszek <jakubantoszek1@gmail.com>
2022-07-07 12:29:05 +0200

    Read Files Commit

--- file.py
+++ file.py
@@ -1,6 +1,7 @@
 import os.path
 import pandas as pd
 
+
 class File:
     def __init__(self, file_path):
         self.__path = file_path
@@ -9,13 +10,13 @@ class File:
         self.__extension = split_path[1]
 
     def get_emails_from_file(self, emails_list):
-        if self.__extension == 'txt':
+        if self.__extension == '.txt':
             with open('emails/' + self.__path) as txt_file:
-                lines = txt_file.readlines()
+                lines = txt_file.read().splitlines()
                 for line in lines:
                     emails_list.append(line)
 
-        if self.__extension == 'csv':
+        if self.__extension == '.csv':
             data = pd.read_csv('emails/' + self.__path, sep=';')
             for row in data['email']:
                 emails_list.append(row)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
+import file
 
 if __name__ == '__main__':
     dir_list = os.listdir('emails')
-    for file in dir_list:
-        print(file)
+    emails_list = []
+
+    for file_path in dir_list:
+        f = file.File(file_path)
+        f.get_emails_from_file(emails_list)
